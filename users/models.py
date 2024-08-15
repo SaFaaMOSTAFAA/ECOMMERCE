0f69accfae3739644a4cda4e4b52a056dc06aaa1
safaamostafa <safamos21@gmail.com>
2024-08-15 16:20:48 +0300

    create 2 fun To make the password encrypted

--- users/models.py
+++ users/models.py
@@ -6,6 +6,6 @@ phone_validator = RegexValidator(regex=r'^\d+$')
 
 class Admin(models.Model):
     full_name = models.CharField(max_length=100)
-    phone = models.IntegerField()
+    phone = models.CharField(validators=[phone_validator], max_length=25)
     user_name = models.CharField(max_length=100)
     password = models.CharField(max_length=50)
--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from rest_framework.exceptions import ValidationError
+from django.contrib.auth.hashers import make_password
 from rest_framework.viewsets import ModelViewSet
 
 from users.models import Admin
@@ -10,15 +10,15 @@ class AdminViewSet(ModelViewSet):
     serializer_class = AdminSerializer
 
     def perform_create(self, serializer):
-        phone = self.request.data.get('phone')
-        if not phone.isdigit():
-            raise ValidationError(
-                {"phone": "Phone number must contain only digits."})
-        serializer.save()
+        password = self.request.data.get('password')
+        if password:
+            serializer.save(password=make_password(password))
+        else:
+            serializer.save()
 
     def perform_update(self, serializer):
-        phone = self.request.data.get('phone')
-        if not phone.isdigit():
-            raise ValidationError(
-                {"phone": "Phone number must contain only digits."})
-        serializer.save()
+        password = self.request.data.get('password')
+        if password:
+            serializer.save(password=make_password(password))
+        else:
+            serializer.save()
