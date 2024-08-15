ec367d6afbce13b81cdd1b4d0e9fa7ff3e7b8571
safaamostafa <safamos21@gmail.com>
2024-08-15 13:08:31 +0300

    add perform_create and perform_update functions to check phone value

--- users/models.py
+++ users/models.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.hashers import make_password
 from django.core.validators import RegexValidator
 from django.db import models
 
@@ -7,11 +6,6 @@ phone_validator = RegexValidator(regex=r'^\d+$')
 
 class Admin(models.Model):
     full_name = models.CharField(max_length=100)
-    phone = models.CharField(validators=[phone_validator], max_length=25)
+    phone = models.IntegerField()
     user_name = models.CharField(max_length=100)
     password = models.CharField(max_length=50)
-
-    def save(self, *args, **kwargs):
-        if not self.password.startswith('pbkdf2_'):
-            self.password = make_password(self.password)
-        super().save(*args, **kwargs)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from rest_framework.exceptions import ValidationError
 from rest_framework.viewsets import ModelViewSet
 
 from users.models import Admin
@@ -7,3 +8,17 @@ from users.serializers import AdminSerializer
 class AdminViewSet(ModelViewSet):
     queryset = Admin.objects.all()
     serializer_class = AdminSerializer
+
+    def perform_create(self, serializer):
+        phone = self.request.data.get('phone')
+        if not phone.isdigit():
+            raise ValidationError(
+                {"phone": "Phone number must contain only digits."})
+        serializer.save()
+
+    def perform_update(self, serializer):
+        phone = self.request.data.get('phone')
+        if not phone.isdigit():
+            raise ValidationError(
+                {"phone": "Phone number must contain only digits."})
+        serializer.save()
