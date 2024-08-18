d9840baa96e51906a10b3bb952cd57e3455230bb
safaamostafa <safamos21@gmail.com>
2024-08-18 15:24:28 +0300

    create postgresql db And connected to it

--- users/models.py
+++ users/models.py
@@ -8,4 +8,4 @@ class Admin(models.Model):
     full_name = models.CharField(max_length=100)
     phone = models.CharField(validators=[phone_validator], max_length=25)
     user_name = models.CharField(max_length=100)
-    password = models.CharField(max_length=50)
+    password = models.CharField(max_length=128)
