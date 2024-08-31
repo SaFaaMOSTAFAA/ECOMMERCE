8b8b8624efab8b505544a2721f080c6726a8e3b8
safaamostafa <safamos21@gmail.com>
2024-08-31 19:19:04 +0300

    rename fields in models file and remove pic

--- cart/models.py
+++ cart/models.py
@@ -6,10 +6,10 @@ from users.models import CustomerAccount
 
 class Cart(models.Model):
     created = models.DateTimeField()
-    customer_id = models.ForeignKey(CustomerAccount, on_delete=models.CASCADE)
+    customer = models.ForeignKey(CustomerAccount, on_delete=models.CASCADE)
 
 
 class CartItem(models.Model):
     product_cost = models.FloatField()
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
-    cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
