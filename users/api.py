f4d036920b8b0751692e6f299d79d3bed251b10c
safaamostafa <safamos21@gmail.com>
2024-09-04 12:12:51 +0300

    update RegisterCustomerAPIView

--- users/api.py
+++ users/api.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.hashers import make_password
-from rest_framework import filters
+from rest_framework import filters, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from users.models import Admin, CustomerAccount, Trader
@@ -66,20 +68,15 @@ class CustomerAccountViewSet(ModelViewSet):
             serializer.save()
 
 
-class RegisterCustomerViewSet(ModelViewSet):
-    queryset = CustomerAccount.objects.all()
-    serializer_class = CustomerAccountSerializer
+class RegisterCustomerAPIView(APIView):
+    def post(self, request, format=None):
+        serializer = CustomerAccountSerializer(data=request.data)
+        if serializer.is_valid():
+            password = self.request.data.get('password')
+            if password:
+                serializer.save(password=make_password(password))
+            else:
+                serializer.save()
 
-    def perform_create(self, serializer):
-        password = self.request.data.get('password')
-        if password:
-            serializer.save(password=make_password(password))
-        else:
-            serializer.save()
-
-    def perform_update(self, serializer):
-        password = self.request.data.get('password')
-        if password:
-            serializer.save(password=make_password(password))
-        else:
-            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,17 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from users.api import (AdminViewSet, CustomerAccountViewSet,
-                       RegisterCustomerViewSet, TraderViewSet)
+                       RegisterCustomerAPIView, TraderViewSet)
 
 router = DefaultRouter()
 router.register(r'admins', AdminViewSet, basename="admins")
 router.register(r'traders', TraderViewSet, basename="traders")
 router.register(r'customer-accounts',
                 CustomerAccountViewSet, basename="customer-accounts")
-router.register(r'register-customers',
-                RegisterCustomerViewSet, basename='register-customers')
 
 urlpatterns = router.urls
+urlpatterns += [
+    path('register-customers/',
+         RegisterCustomerAPIView.as_view(), name='register-customers'),
+]
