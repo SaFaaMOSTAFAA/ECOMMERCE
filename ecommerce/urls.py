12802ada498186918be6348a642bf5c8d3ed72af
safaamostafa <safamos21@gmail.com>
2024-09-22 18:36:18 +0300

    add requirements, url and settings files

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,6 +1,7 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 
@@ -14,4 +15,7 @@ urlpatterns = [
          TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/',
          TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(
+        url_name='schema'), name='swagger-ui')
 ]
