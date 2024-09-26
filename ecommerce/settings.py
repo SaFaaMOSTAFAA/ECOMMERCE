eb568bbcb42131856763a7983b5f656aa49736bc
SaFaaMOSTAFAA <148200912+SaFaaMOSTAFAA@users.noreply.github.com>
2024-09-26 15:38:17 +0300

    send email after create trader (#35)
    * send email after create trader
    
    * update settings file
    
    * add response to perform create and update

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -67,7 +67,20 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
      ),
 }
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.office365.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'wpbawomenhealth@outlook.com'
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = 'wpbawomenhealth@outlook.com'
+
+
 AUTH_USER_MODEL = 'users.UserAccount'
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
