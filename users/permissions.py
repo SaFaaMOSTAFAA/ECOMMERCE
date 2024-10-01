c070f625497614d63d71b2167d6e5ba38042ddaa
safaamostafa <safamos21@gmail.com>
2024-10-01 14:38:29 +0300

    remove noqa

--- users/permissions.py
+++ users/permissions.py
@@ -3,4 +3,5 @@ from rest_framework.permissions import BasePermission
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.get_role() == 'admin' # noqa
+        return request.user.is_authenticated \
+               and request.user.get_role() == 'admin'
