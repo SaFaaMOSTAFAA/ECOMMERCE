6c794752a2c0337a7c1f25ec5a5a04c456f62654
safaamostafa <safamos21@gmail.com>
2024-10-01 18:20:32 +0300

    create pytest for users app

---
+++ users/test_users.py
@@ -0,0 +1,77 @@
+import pytest
+from django.test import Client
+from django.urls import reverse
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from users.models import Admin, Trader
+
+
+@pytest.mark.django_db
+class TestTraderAPI:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.admin = Admin.objects.create(
+            full_name='admin1', phone='01012070620',
+            user_name='admin1', password='admin',
+            email='admin1@gmail.com', is_active=True, is_staff=True
+        )
+
+        self.trader = Trader.objects.create(
+            full_name='trader1', phone='01012070620',
+            user_name='trader1', password='admin',
+            email='trader1@gmail.com', is_active=True,
+            is_staff=False, address='address1'
+        )
+        self.client = Client()
+        refresh = RefreshToken.for_user(self.admin)
+        access_token = str(refresh.access_token)
+        self.client.defaults['HTTP_AUTHORIZATION'] = f'Bearer {access_token}'
+        self.url_list = reverse("traders-list")
+        self.url_detail = reverse("traders-detail", args=[self.trader.id])
+
+    def test_list_trader(self):
+        response = self.client.get(self.url_list)
+        assert response.status_code == 200
+
+    def test_create_trader(self):
+        data = {
+            'full_name': 'trader2', 'phone': '01012070620',
+            'user_name': 'trader2', 'password': 'admin',
+            'email': 'trader1@gmail.com', 'is_active': True,
+            'is_staff': False, 'address': 'address2'
+        }
+        response = self.client.post(
+            self.url_list, data=data, content_type='application/json'
+        )
+        assert response.status_code == 201
+
+    def test_update_trader(self):
+        update_data = {
+            'full_name': 'trader_2', 'phone': '01012070620',
+            'user_name': 'trader_2', 'password': 'trader',
+            'email': 'trader1@gmail.com', 'is_active': True,
+            'is_staff': False, 'address': 'address_2'
+        }
+        response = self.client.put(
+            self.url_detail, data=update_data, content_type='application/json')
+        assert response.status_code == 200
+
+    def test_retrieve_trader(self):
+        update_data = {
+            'full_name': 'trader_2', 'phone': '01012070620',
+            'password': 'admin', 'email': 'trader1@gmail.com',
+            'user_name': 'trader_2', 'is_active': True,
+            'is_staff': False, 'address': 'address_2'
+        }
+        response = self.client.patch(
+            self.url_detail,
+            data=update_data,
+            content_type='application/json'
+        )
+        assert response.status_code == 200
+
+    def test_delete_trader(self):
+        response = self.client.delete(
+            self.url_detail
+        )
+        assert response.status_code == 204
