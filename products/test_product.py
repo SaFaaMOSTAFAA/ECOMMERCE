985432aff994bd3273b9b690224eb69334378f45
SaFaaMOSTAFAA <148200912+SaFaaMOSTAFAA@users.noreply.github.com>
2024-10-26 14:14:14 +0300

    create unit test for product app (#50)
    * create unit test for product app
    
    * fix test update product
    
    * remove print & check deleted product with all_objects manager

---
+++ products/test_product.py
@@ -0,0 +1,79 @@
+import tempfile
+
+import pytest
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import Client
+from django.urls import reverse
+from PIL import Image
+
+from products.models import Brand, Category, Product
+
+
+@pytest.mark.django_db
+class TestProducttAPI:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.brand = Brand.objects.create(name='brand1')
+        self.category = Category.objects.create(name='category1')
+        with tempfile.NamedTemporaryFile(suffix='.png') as tmp:
+            image = Image.new('RGB', (100, 100), color='red')
+            image.save(tmp, format='PNG')
+            tmp.seek(0)
+
+            uploaded_image = SimpleUploadedFile(
+                name='test_image.png', content=tmp.read(),
+                content_type='image/png')
+
+        self.product = Product.objects.create(
+            name='product1', price='55.55', quantity='6',
+            image=uploaded_image, purchase_price='66.55',
+            category=self.category, brand=self.brand)
+        self.client = Client()
+        self.url_list = reverse("products-list")
+        self.url_detail = reverse("products-detail", args=[self.product.id])
+
+    def test_list_product(self):
+        response = self.client.get(self.url_list)
+        assert response.status_code == 200
+
+    def test_create_product(self):
+        with tempfile.NamedTemporaryFile(suffix='.png') as tmp:
+            image = Image.new('RGB', (100, 100), color='red')
+            image.save(tmp, format='PNG')
+            tmp.seek(0)
+            uploaded_image = SimpleUploadedFile(
+                name='test_image.png', content=tmp.read(),
+                content_type='image/png')
+        data = {
+            'name': 'product1', 'price': '55.55', 'quantity': '6',
+            'image': uploaded_image, 'purchase_price': '66.55',
+            'category': self.category.id, 'brand': self.brand.id
+        }
+        response = self.client.post(
+            self.url_list, data=data, format='multipart')
+        assert response.status_code == 201
+
+    def test_update_product(self):
+        with tempfile.NamedTemporaryFile(suffix='.png') as tmp:
+            image = Image.new('RGB', (100, 100), color='red')
+            image.save(tmp, format='PNG')
+            tmp.seek(0)
+        update_data = {
+            'name': 'product1', 'price': '58.55', 'quantity': '8',
+            'purchase_price': '66.55',
+            'category': self.category.id, 'brand': self.brand.id
+        }
+        response = self.client.patch(self.url_detail,
+                                     data=update_data,
+                                     content_type='application/json')
+        assert response.status_code == 200
+
+    def test_delete_product(self):
+        response = self.client.delete(self.url_detail)
+        assert response.status_code == 204
+        product_exists_in_default = Product.objects.filter(id=self.product.id).exists()
+        assert not product_exists_in_default
+        product_exists_in_all_objects = Product.all_objects.filter(id=self.product.id).exists()
+        assert product_exists_in_all_objects
+        deleted_product = Product.all_objects.get(id=self.product.id)
+        assert deleted_product.deleted_at is not None
